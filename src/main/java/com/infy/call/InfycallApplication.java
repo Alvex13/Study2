6a4f3b77eb0ef71b1291e7c1cfe6d8019fbff4cf
Alvin V <alvin.v@ad.infosys.com>
2024-04-29 20:39:55 +0530

    initial commit

---
+++ src/main/java/com/infy/call/InfycallApplication.java
@@ -0,0 +1,13 @@
+package com.infy.call;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfycallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfycallApplication.class, args);
+	}
+
+}
